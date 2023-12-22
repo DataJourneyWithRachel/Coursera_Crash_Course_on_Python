8e3c13d4939407bc4bf6b4f9e69961f8507a5a17
Rachel <149018958+rachellin94@users.noreply.github.com>
2023-12-22 12:00:56 -0800

    Update Module3_Whileloops.py

--- Module3_Whileloops.py
+++ Module3_Whileloops.py
@@ -1,1 +1,12 @@
 
+# In Python, what do while loops do?
+# Ans :
+# while loops tell the computer to repeatedly execute a set of instructions while a condition is true.
+
+# Which techniques can prevent an infinite while loop? Select all that apply.
+# Ans :
+# Change the value of a variable used in the while condition
+# Use the break keyword
+
+
+
